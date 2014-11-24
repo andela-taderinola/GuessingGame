210f236269f715337236144eb1765ab5e7a6e673
Timilehin Aderinola <timiderinola@gmail.com>
2014-11-24 18:04:06 +0100

    Third Commit

--- js/obj.js
+++ js/obj.js
@@ -1,19 +1,46 @@
 
-var hiddenNumber = Math.round(Math.random() * 100);
-console.log('I have picked a number bewteen 1 and 100');
-    console.log(hiddenNumber);
+// console.log('I have picked a number bewteen 1 and 100');
+// console.log(hiddenNumber);
 var previousGuess = null;
 var guessNumber = 0;
 var guess;
 var feedbackMsg;
+var guessGap;
 
 var GuessingGame = {
   checkButton: document.getElementById('check'),
   newGameButton: document.getElementById('new-game'),
   guessInput: document.getElementById('guess-input'),
   feedbackLabel: document.getElementById('feedback'),
+  thermocheck: document.getElementById('thermo'),
+
+  showTemp: function() {
+
+    if(guessGap >= 80){
+      GuessingGame.thermocheck.setAttribute("class", "temp80");
+      console.log("Gap is " + guessGap);
+    } else if(guessGap >= 60){
+      GuessingGame.thermocheck.setAttribute("class","temp60");
+      console.log("Gap is " + guessGap);
+    } else if(guessGap >= 40){
+      GuessingGame.thermocheck.setAttribute("class","temp40");
+      console.log("Gap is " + guessGap);
+    } else if(guessGap >= 20){
+      GuessingGame.thermocheck.setAttribute("class","temp20");
+      console.log("Gap is " + guessGap);
+    } else if(guessGap > 0){
+      GuessingGame.thermocheck.setAttribute("class","temp10");
+      console.log("Gap is " + guessGap);
+    } else if(guessGap == 0) {
+      GuessingGame.thermocheck.setAttribute("class","boiled");
+      console.log("Gap is " + guessGap);
+    }
+
+  },
 
   chooseNumber: function() {
+    GuessingGame.thermocheck.setAttribute("class","thermoDemo");
+    previousGuess = null;
     GuessingGame.guessInput.value = "";
     GuessingGame.checkButton.disabled = false;
     hiddenNumber = Math.round(Math.random() * 100);
@@ -23,6 +50,7 @@ var GuessingGame = {
   },
 
   checkGuess: function() {
+
     var input = GuessingGame.guessInput.value;
     console.log(hiddenNumber);
     guess = parseInt(input);
@@ -33,6 +61,7 @@ var GuessingGame = {
     return;
     }  
 
+    guessGap = Math.abs(hiddenNumber - guess);
     GuessingGame.guessInput.value = "";
     guessNumber++; 
 
@@ -43,20 +72,25 @@ var GuessingGame = {
 
       if(previousGuess === null){
         previousGuess = guess;
-        GuessingGame.feedbackLabel.innerText = "Nah...nice try. Try again!";
+        GuessingGame.feedbackLabel.innerText = "Nice first try. Try again!";
+        GuessingGame.showTemp();
       } else if(previousGuess === guess) {
-        GuessingGame.feedbackLabel.innerText = "Didn't I tell you that was a wrong guess?! You've gotta do better than this!";
+        GuessingGame.feedbackLabel.innerText = "I heard you the first time. Why repeat yourself?";
         // previousGuess = guess;
+        GuessingGame.showTemp();
       } else {
-         var guessGap = Math.abs(hiddenNumber - guess);
+         
          var previousGuessGap = Math.abs(hiddenNumber - previousGuess);
 
           if(guessGap > previousGuessGap) {
-            GuessingGame.feedbackLabel.innerText = "Huh huh... you're getting colder";
+            GuessingGame.feedbackLabel.innerText = "You're getting colder.";
+            GuessingGame.showTemp();
           } else if(guessGap < previousGuessGap) {
-            GuessingGame.feedbackLabel.innerText = "Yeah... you're getting hotter!";
+            GuessingGame.feedbackLabel.innerText = "You're getting hotter!";
+            GuessingGame.showTemp();
           } else {
-            GuessingGame.feedbackLabel.innerText = "Hmm... lukewarm. Neither hot nor cold.";
+            GuessingGame.feedbackLabel.innerText = "I don't feel you. C'mon! You can do it!";
+            GuessingGame.showTemp();
           }
 
           previousGuess = guess;
@@ -67,6 +101,7 @@ var GuessingGame = {
     } else {
         GuessingGame.checkButton.disabled = true;
         GuessingGame.feedbackLabel.innerText = "Ouch! You got me in " + guessNumber + " guesses!";
+        GuessingGame.showTemp();
         // GuessingGame.giveFeedback(feedbackMsg);
         return;
       }    
@@ -78,6 +113,6 @@ var GuessingGame = {
   //   return;
   // }
 }
-
   GuessingGame.checkButton.onclick = GuessingGame.checkGuess;
   GuessingGame.newGameButton.onclick = GuessingGame.chooseNumber;
+  window.onload = GuessingGame.chooseNumber;
